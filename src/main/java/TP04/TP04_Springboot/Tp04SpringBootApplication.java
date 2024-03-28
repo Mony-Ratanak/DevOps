16a6a63a714e8af6b28e6c9ccbd005939dfd4af9
Mony Ratanak <hieklymonyratanak@gmail.com>
2024-03-28 18:45:21 +0700

    Change project

---
+++ src/main/java/TP04/TP04_Springboot/Tp04SpringBootApplication.java
@@ -0,0 +1,13 @@
+package TP04.TP04_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp04SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp04SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TP04/TP04_Springboot/Tp04SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package TP04.TP04_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp04SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
